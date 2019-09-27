4bfc235ccde15894b38c2ba77fc4d6e9e40ed6a6
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2019-09-27 12:25:59 +0200

    test multiarch release

--- script/release/release-containerd.sh
+++ script/release/release-containerd.sh
@@ -26,24 +26,32 @@ install_dependencies() {
 }
 
 # Add repositories with multiple architectures
+source /etc/os-release
 cat <<EOF > /etc/apt/sources.list
-deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ bionic main multiverse restricted universe
-deb [arch=armhf,arm64,ppc64el,s390x] http://ports.ubuntu.com/ubuntu-ports/ bionic main multiverse restricted universe
-deb [arch=armhf,arm64,ppc64el,s390x] http://ports.ubuntu.com/ubuntu-ports/ bionic-updates main multiverse restricted universe
-deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ bionic-updates main multiverse restricted universe
-deb [arch=amd64] http://security.ubuntu.com/ubuntu/ bionic-security main multiverse restricted universe
+deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ ${VERSION_CODENAME} main multiverse restricted universe
+deb [arch=armhf,arm64,ppc64el,s390x] http://ports.ubuntu.com/ubuntu-ports/ ${VERSION_CODENAME} main multiverse restricted universe
+deb [arch=armhf,arm64,ppc64el,s390x] http://ports.ubuntu.com/ubuntu-ports/ ${VERSION_CODENAME}-updates main multiverse restricted universe
+deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ ${VERSION_CODENAME}-updates main multiverse restricted universe
+deb [arch=amd64] http://security.ubuntu.com/ubuntu/ ${VERSION_CODENAME}-security main multiverse restricted universe
 EOF
 
 apt-get update
 
-# Cross compile for the following architectures
+# Create amd64 release
+echo "Creating amd64 release ..."
+make release
+
+# Cross compile for the other architectures
 CONTAINERD_ARCH=(
-    amd64
     arm
     arm64
     ppc64le
     s390x
 )
+
+# Remove libssecomp shared libraries from default location
+# to avoid conflicts crosscompiling
+rm /usr/local/lib/libseccomp*
 
 for arch in "${CONTAINERD_ARCH[@]}"; do
     make clean
@@ -51,26 +59,28 @@ for arch in "${CONTAINERD_ARCH[@]}"; do
     # and install dependencies
     case ${arch} in
     arm)
-      install_dependencies("armhf")
-      CC="arm-linux-gnueabihf-gcc"
+      install_dependencies "armhf"
+      ARCH_PREFIX="arm-linux-gnueabihf"
       ;;
     arm64)
-      install_dependencies("arm64")
-      CC="aarch64-linux-gnu-gcc"
+      install_dependencies "arm64"
+      ARCH_PREFIX="aarch64-linux-gnu"
       ;;
     ppc64le)
-      install_dependencies("ppc64le")
-      CC="powerpc64le-linux-gnu-gcc" 
+      install_dependencies "ppc64el"
+      ARCH_PREFIX="powerpc64le-linux-gnu"
       ;;
     s390x)
-      install_dependencies("s390x")
-      CC="s390x-linux-gnu-gcc" 
-      ;;
-    amd64)
-      unset CC
+      install_dependencies "s390x"
+      ARCH_PREFIX="s390x-linux-gnu"
       ;;
     esac
 
-    make release GOARCH=${arch} CGO_ENABLED=1
+    echo "Creating ${arch} release ..."
+    LD_LIBRARY_PATH=/usr/lib/${ARCH_PREFIX} \
+    make release \
+        GOARCH=${arch} \
+        CC=${ARCH_PREFIX}-gcc \
+        CGO_ENABLED=1
 done
 
